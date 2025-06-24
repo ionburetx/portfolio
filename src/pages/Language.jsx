d898cc9a8f67d899b5306191f63113d042ce4784
ionburetx <ionburgoa1@gmail.com>
2025-06-24 12:32:25 +0200

    hago la rutas relativas

--- src/pages/Language.jsx
+++ src/pages/Language.jsx
@@ -5,14 +5,14 @@ const LanguageScreen = () => {
     <div className="relative w-screen h-screen overflow-hidden">
       {/* Imagen de fondo */}
       <img
-        src="src/assets/generalAssets/fondoBienvenida.png"
+        src="./src/assets/generalAssets/fondoBienvenida.png"
         alt="Fondo"
         className="absolute inset-0 w-full h-full object-cover object-bottom object-right z-0"
 />
 
       {/* Capa con recortes (imagen negra con eu/es/en) */}
       <img
-        src="src/assets/generalAssets/idiomas.png"
+        src="./src/assets/generalAssets/idiomas.png"
         alt="Idioma Mask"
         className="absolute inset-0 w-full h-full object-cover opacity-60 z-10"
 />
